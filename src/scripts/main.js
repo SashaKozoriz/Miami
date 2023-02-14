2c0569a7a75b3167a8ee1d82572f61656f2aa594
Sasha Kozoriz <sashakozoriz2012@gmail.com>
2023-02-14 06:39:30 +0200

    Miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 20) {
+    document
+      .querySelector('.back-to-top')
+      .classList.add('back-to-top--visible');
+  } else {
+    document
+      .querySelector('.back-to-top')
+      .classList.remove('back-to-top--visible');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
